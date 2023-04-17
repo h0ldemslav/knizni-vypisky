58e2bbd5832379a0b742395a1ac936f5b9c4cad3
Petr Chatrný <xchatrny@mendelu.cz>
2023-04-17 15:59:33 +0200

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+import {createVuetify, ThemeDefinition} from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // import Vuetify styles
+
+
+const primaryTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        background: '#696969',
+        surface: '#FFFFFF',
+        primary: '#E4573D',
+        secondary: '#F4F3EC',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
+
+export default createVuetify({
+    theme: {
+        defaultTheme: "primaryTheme",
+        themes: {
+            primaryTheme
+        }
+    }
+})
